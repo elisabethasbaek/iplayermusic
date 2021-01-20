c2159ab50c839aa830a342ce745608c3c7d4458e
elisabethasbaek <elisabeth.asbaek@gmail.com>
2021-01-20 09:02:09 +0100

    add basic router setup and css variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import {Router} from "@reach/router"
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       
-    </div>
+    </Router>
   );
 }
 
