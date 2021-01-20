4b31e6320750ae83fef71b13f0ce87ee904d6242
elisabethasbaek <elisabeth.asbaek@gmail.com>
2021-01-20 13:25:33 +0100

    add new variable for padding

---
+++ src/components/MainNav.js
@@ -0,0 +1,13 @@
+import "../components/style/MainNav.css";
+
+export default function MainNav(){
+    return(
+        <section className="mainNav">
+            <img src="../Pulse.svg" alt="Icon of pulse"/>
+            <img src="../Microphone.svg" alt="Icon of a microphone"/>
+            <img src="../Wifi.svg" alt="Main icon"/>
+            <img src="../Contrast.svg" alt="Icon for light and dark mode"/>
+            <img src="../Sett.svg" alt="Icon for settings"/>
+        </section>
+    )
+}
