ead0174adfb60466ce12afbc695d12ec4e526a7b
elisabethasbaek <elisabeth.asbaek@gmail.com>
2021-01-21 13:00:53 +0100

    dimmeduttiprutti

--- src/components/PlayerImage.js
+++ src/components/PlayerImage.js
@@ -3,7 +3,7 @@ import "../components/style/PlayerImage.css";
 export default function PlayerImage(){
     return(
         <article className="playerImage">
-            <img src="../Record.svg" alt="A record" className="playerImage__image"/>
+            <img src="https://via.placeholder.com/900x900" alt="A record" className="playerImage__image"/>
             <div className="playerImage__border1"></div>
             <div className="playerImage__border2"></div>
         </article>
--- src/pages/Player.js
+++ src/pages/Player.js
@@ -1,6 +1,7 @@
 import "../Variables.css";
 import BreadcrumbNavigation from "../components/BreadcrumbNavigation"; /* component */
 import PlayerImage from "../components/PlayerImage"; /* component */
+import MainNav from "../components/MainNav"; /* component */
 import "../components/style/Main.css"; /* css */
 import "../components/style/Player.css"; /* css */
 import "../Variables.css"; /* css */
@@ -8,12 +9,13 @@ import "../Variables.css"; /* css */
 export default function Player(){
     return(
         <main className="main player">
-            <BreadcrumbNavigation color="rgba(0,0,0,0.0)">Playing</BreadcrumbNavigation>
+            <BreadcrumbNavigation color="rgba(0,0,0,0.0)" display="none">Playing</BreadcrumbNavigation>
             <PlayerImage />
             <section className="player__textContainer">
                 <h1 className="player__title">Title of dope ass song</h1>
                 <h2 className="player__artist">The fucking artist bruh</h2>
             </section>
+            <MainNav />
         </main>
     )
 };
