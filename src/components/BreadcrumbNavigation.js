f1e33550bfbfed8aebd78b6e51ebcb40fa926ded
elisabethasbaek <elisabeth.asbaek@gmail.com>
2021-01-26 12:42:57 +0100

    add back button now works

--- src/components/BreadcrumbNavigation.js
+++ src/components/BreadcrumbNavigation.js
@@ -3,7 +3,7 @@ import "../components/style/BreadcrumbNavigation.css";
 export default function BreadcrumbNavigation(props){
     return(
         <section className="breadcrumbNavigation" style={{backgroundColor:props.color}}>
-            <ion-icon name="chevron-back-outline" className="breadcrumbNavigation__chevron"></ion-icon>
+            <ion-icon name="chevron-back-outline" className="breadcrumbNavigation__chevron" onClick={() => window.history.back()}></ion-icon>
             <h1 className="breadcrumbNavigation__location">{props.children}</h1>
             <ion-icon name="search-outline" className="breadcrumbNavigation__search" style={{display:props.display}}></ion-icon>
         </section>
