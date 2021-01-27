9b2061ec529568f8e2fd0338c04b2b2cda119b7f
elisabethasbaek <elisabeth.asbaek@gmail.com>
2021-01-27 18:57:25 +0100

    this shit is commit c o m m i t

--- .netlify/functions/token.js
+++ .netlify/functions/token.js
@@ -1,11 +1,36 @@
+const { default: axios } = require("axios");
+
 /* netlify function */
-exports.handler = function(event, context){
-    if(event.httpMethod !== "POST") return { statusCode: 405 };
-    
+exports.handler = async function(event, context){
+    if(event.httpMethod !== "POST") return { statusCode: 405, body: "" };
+
     var body = JSON.parse(event.body);
 
-    return {
-        statusCode: 200,
-        body: ""
+    var authOptions = {
+        url: "https://accounts.spotify.com/api/token",
+        method: "post",
+        params: {
+            code: body.code,
+            redirect_uri: process.env.REDIRECT_URI,
+            grant_type: "authorization_code"
+        },
+        headers: {
+            "Authorization": "Basic " + (Buffer.from(process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET).toString("base64"))
+        }
+    }
+
+    try{
+        var response = await axios(authOptions);
+
+        return {
+            statusCode: 201,
+            body: JSON.stringify(response.data)
+        }
+    } catch (error){
+        console.log(error);
+        return {
+            statusCode: 500,
+            body: "Internal server error"
+        }
     }
 }
