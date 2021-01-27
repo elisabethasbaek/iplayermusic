aa3c60896cc8158b50477a185c9c407eebcf7d12
elisabethasbaek <elisabeth.asbaek@gmail.com>
2021-01-27 18:11:11 +0100

    this is a commit

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,3 +1,4 @@
+import queryString from "querystring";
 import "../Variables.css"; /* css */
 import "../components/style/LogIn.css"; /* css */
 import Heading from "../components/Heading"; /* component */
@@ -5,6 +6,14 @@ import PlaylistsButton from "../components/PlaylistsButton";
 import LogInField from "../components/LogInField";
 
 export default function LogIn(){
+    var queryParameters = queryString.stringify({
+        response_type: "code",
+        client_id: "de5abe58dab743b786d000caeabc78e1",
+        scope: "user-read-private user-read-email",
+        redirect_uri: "http://localhost:8888/callback",
+        state: "jfedvpusebvfpesbfvpief893y4937y647yriugfb3i78tr873ugr3urbvfbpu3rgr82y210"
+    });
+
     return(
         <main className="main login">
             <Heading>Log In</Heading>
@@ -12,6 +21,9 @@ export default function LogIn(){
                 <LogInField forr="username" type="text" placeholder="Enter your username" icon="../User.svg">Username</LogInField>
                 <LogInField forr="password" type="password" placeholder="Enter your password" icon="../Key.svg">Password</LogInField>
             </form>
+
+            <a href={`https://accounts.spotify.com/authorize?${queryParameters}`}>Log in with Spotify</a> {/* style me oh god please */}
+
             <PlaylistsButton text="Log In" album="/" />
             <article className="login__oneTouch">
                 <img src="../Touch.svg" alt="Touch for one-touch login"/>
